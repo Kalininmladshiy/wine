b0bac593e08327b48804be0f7a16f01046a3dfc3
Максим Калинин <maksim.kalinin1@gmail.com>
2022-10-13 14:41:46 +0300

    Changed function name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from utils import noun_form, get_age, get_drinks
+from utils import noun_form, get_age, get_drinks_by_category
 
 
 if __name__ == '__main__':
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     template = env.get_template('template.html')
     
     winery_age=get_age()
-    drinks_by_category = get_drinks()
+    drinks_by_category = get_drinks_by_category()
     rendered_page = template.render(
         winery_age=winery_age,
         drinks_by_category=drinks_by_category,
